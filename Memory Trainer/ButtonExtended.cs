4c795661372f82184dc01628ac6be86ec6a71c9c
Derek Gooding <djgooding@live.com>
2021-09-29 15:15:16 -0400

    Initial commit

---
+++ Memory Trainer/ButtonExtended.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace Memory_Trainer
+{
+    class ButtonExtended
+    {
+        private Button MyButton { get; set; }
+        private string ID { get; set; }
+        private MainWindow MyWindow { get; set; }
+        private readonly ColorAnimation colorAnimation;
+        private readonly Storyboard storyboard;
+        private Brush defaultColor = Brushes.LightGray;
+        private readonly Brush correctColor = Brushes.Green;
+        private readonly Brush failColor = Brushes.Red;
+
+        public ButtonExtended(string xID, string yID, Button button, MainWindow mainWindow, bool isOdd)
+        {
+            if (isOdd)
+                defaultColor = Brushes.DarkGray;
+            ID = xID + yID;
+            MyButton = button;
+            MyButton.Click += ReturnID;
+            MyWindow = mainWindow;
+            button.Background = defaultColor;
+
+            colorAnimation = new ColorAnimation()
+            {
+                Duration = TimeSpan.FromSeconds(5),
+                To = ((SolidColorBrush)defaultColor).Color
+            };
+            storyboard = new Storyboard();
+            storyboard.Children.Add(colorAnimation);
+            Storyboard.SetTarget(colorAnimation, MyButton);
+            Storyboard.SetTargetProperty(colorAnimation, new PropertyPath("(Button.Background).(SolidColorBrush.Color)"));
+        }
+
+        private void ReturnID(object sender, RoutedEventArgs e)
+        {
+
+            if (ID == MyWindow.Target)
+            {
+                SetColor(correctColor);
+                MyWindow.L_Output.Content = "Correct";
+            }
+            else
+            {
+                SetColor(failColor);
+                MyWindow.L_Output.Content = "Fail";
+            }
+            storyboard.Begin();
+            MyWindow.RandomizeTarget();
+        }
+
+        private void SetColor(Brush color)
+        {
+            colorAnimation.From = ((SolidColorBrush)color).Color;
+            MyButton.Background = color;
+            MyWindow.L_Output.Foreground = color;
+        }
+    }
+}
---
+++ Memory Trainer/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Speech.Synthesis;
+using System.Speech.AudioFormat;
+using System.IO;
+using System.Linq;
+
+namespace Memory_Trainer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly int size = 8;
+        public MainWindow()
+        {
+            InitializeComponent();
+            PrepareBoard();
+            GetVoice();
+            RandomizeTarget();
+        }
+        void PrepareBoard()
+        {
+            for (int i = size; i >= 1; i--)
+                SP_Grid.Children.Add(CreateRow(i));
+        }
+
+        StackPanel CreateRow(int yValue)
+        {
+            var output = new StackPanel
+            {
+                Orientation = Orientation.Horizontal
+            };
+            for (int i = 1; i <= size; i++)
+                output.Children.Add(CreateCell(i, yValue));
+            return output;
+        }
+
+        Button CreateCell(int xValue, int yValue)
+        {
+            var myWidth = G_Grid.Width / size;
+            var myHeight = G_Grid.Height / size;
+            var output = new Button
+            {
+                Height = myHeight,
+                Width = myWidth
+            };
+            bool isOdd = false;
+            if ((xValue + yValue) % 2 == 0) isOdd = true;
+            new ButtonExtended(ConvertToLetter(xValue), yValue.ToString(), output, this, isOdd);
+            return output;
+        }
+
+        string ConvertToLetter(int input)
+        {
+            var output = 'A';
+            for (int i = 0; i < input - 1; i++)
+            {
+                output++;
+            }
+            return output.ToString();
+        }
+        string target;
+        private readonly Random rnd = new Random();
+        public string Target { get => target;}
+        public void RandomizeTarget()
+        {
+            target = ConvertToLetter(rnd.Next(1, size + 1)) + rnd.Next(1, size + 1);
+            L_Target.Content = target;
+            TextToSpeech(target);
+        }
+
+        string CurrentVoice;
+        private void GetVoice()
+        {
+            var synthesizer = new SpeechSynthesizer();
+            var voiceCollection = synthesizer.GetInstalledVoices();
+            CurrentVoice = voiceCollection.First().VoiceInfo.Name;
+        }
+
+        private void TextToSpeech(string message)
+        {
+            string[] seperator = { "A" };
+            if(message.Contains("A"))
+            {
+                var temp = message.Split(seperator, StringSplitOptions.RemoveEmptyEntries);
+                message = "A. " + temp[0];
+            }
+            Thread thread = new Thread(() =>
+            {
+                var synthesizer = new SpeechSynthesizer();
+                synthesizer.SetOutputToDefaultAudioDevice();
+                synthesizer.SelectVoice(CurrentVoice);
+                synthesizer.Speak(message);
+            });
+            thread.Start();
+        }
+    }
+}
